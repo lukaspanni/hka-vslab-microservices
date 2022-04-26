a081aad545ad549612ee23565474a55c77f24b30
Lukas Panni <lukas-panni@web.de>
2022-04-26 13:44:45 +0200

    ProductService: check if category exists

---
+++ product-service/src/main/java/de/hkamicroservices/productservice/CategoryDto.java
@@ -0,0 +1,27 @@
+package de.hkamicroservices.productservice;
+
+public class CategoryDto {
+
+    Long id;
+    String name;
+
+    public CategoryDto(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- product-service/src/main/java/de/hkamicroservices/productservice/ProductController.java
+++ product-service/src/main/java/de/hkamicroservices/productservice/ProductController.java
@@ -1,16 +1,23 @@
 package de.hkamicroservices.productservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Objects;
 
 
 @RestController()
 @RequestMapping(path = "/products")
 public class ProductController {
 
+    private final String categoryServiceEndpoint = Objects.equals(System.getenv("CATEGORY_ENDPOINT"), "") ? System.getenv("CATEGORY_ENDPOINT") : "localhost";
     private final ProductRepository productRepository;
 
     @Autowired
@@ -31,9 +38,25 @@ public class ProductController {
     @PostMapping(path = "/", consumes = {"application/json"}, produces = {"application/json"})
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        if (product.getCategoryId() != 0 && !checkCategoryExists(product.getCategoryId()))
+            return ResponseEntity.badRequest().body("Category does not exist");
+
+
         var createdProduct = productRepository.save(product);
         var location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdProduct.getId()).toUri();
         return ResponseEntity.created(location).body(createdProduct);
+    }
+
+    private boolean checkCategoryExists(int categoryId) {
+        WebClient client = WebClient.create("http://" + categoryServiceEndpoint + ":8080/categories/");
+        try {
+            client.get().uri(String.valueOf(categoryId)).retrieve().bodyToMono(CategoryDto.class).block();
+        } catch (WebClientResponseException wcre) {
+            if (wcre.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                return false;
+            throw wcre;
+        }
+        return true;
     }
 
     @DeleteMapping(path = "/{id}")
